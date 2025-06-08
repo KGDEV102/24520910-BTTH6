052fbec27b15e95bafe1b1f9e4aa4af88b0818f4
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-06-08 19:17:50 +0700

    Create main.cpp

---
+++ Bai3/main.cpp
@@ -0,0 +1,12 @@
+#include"QUANLYSINHVIEN.h"
+int main() {
+	QUANLYSINHVIEN ds;
+	ds.Nhap();
+	ds.Xuat();
+	ds.SVDuDKTN();
+	ds.SVKhongDuDKTN();
+	ds.SVCDMax();
+	ds.SVDHMax();
+	ds.SLSVKhongDuDKTN();
+	return 0;
+}
