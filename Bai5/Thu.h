5e1a8939f31d8d0449efb172d14515c20fa77a9c
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-06-08 19:23:17 +0700

    Create Thu.h

---
+++ Bai5/Thu.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Thu
+{
+protected:
+public:
+	virtual int SinhCon() = 0;
+	virtual int ChoSua() = 0;
+	virtual string Keu() = 0;
+
+};
+
