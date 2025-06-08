c5210248df41f48fcaa3ddba8e4aecded48deebf
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-06-08 19:12:49 +0700

    Create GiaoDichNhaPho.h

---
+++ Bai2/GiaoDichNhaPho.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"GiaoDich.h"
+class GiaoDichNhaPho :public GiaoDich
+{
+protected:
+	int LoaiNha;
+	string DiaChi;
+
+public:
+	void Nhap();
+	void Xuat();
+	double ThanhTien();
+	string GetLoai();
+	string GetNgay();
+};
