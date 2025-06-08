775e0f6b862e4c499f38d23fc5c65ade6b3a653c
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-06-08 19:24:14 +0700

    Create NONGTRAI.h

---
+++ Bai5/NONGTRAI.h
@@ -0,0 +1,16 @@
+#pragma once
+#include"Thu.h"
+class NONGTRAI
+{
+private:
+	int sl;
+	Thu** dst;
+public:
+	~NONGTRAI();
+	NONGTRAI();
+	void Nhap();
+	void Xuat();
+	int TongLuongSua();
+	int TongConSinhRa();
+};
+
